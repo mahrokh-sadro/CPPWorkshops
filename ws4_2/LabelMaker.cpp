8c15bd5768f2908ee7a7b4cc4f951709125f25d8
mahrokh sadro <74023410+mahrokh-sadro@users.noreply.github.com>
2021-02-13 16:39:12 -0500

    Update LabelMaker.cpp

--- ws4_2/LabelMaker.cpp
+++ ws4_2/LabelMaker.cpp
@@ -39,11 +39,11 @@ namespace sdds {
 	void LabelMaker::readLabels()	{
 		cout << "Enter" << m_noOfLabels << "labels:" << endl;
 
-
 		int i(0);
 		for (i = 0; i < m_noOfLabels; i++) {
 
-			cout << "Enter label number (i+1)" << endl;
+			cout << "Enter label number"<< i + 1 << endl;
+			cout << ">";
 			m_labels[i].readLabel();
 		}
 	}
