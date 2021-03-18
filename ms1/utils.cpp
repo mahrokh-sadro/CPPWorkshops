43007392fcfb155d017630af5e8a60ee9ae5970c
mahrokh sadro <74023410+mahrokh-sadro@users.noreply.github.com>
2021-03-18 06:18:34 -0400

    Add files via upload

--- ms1/utils.cpp
+++ ms1/utils.cpp
@@ -6,142 +6,177 @@
 #include <cstring>
 #include "utils.h"
 #include "Time.h"
+
+
+#include <cstdio>
 using namespace std;
 namespace sdds {
     bool debug = false;  // made global in utils.h
-    int getTime() {  
-       int mins = -1;
-       if (debug) {
-          Time t(0);
-          cout << "Enter current time: ";
-          do {
-             cin.clear();
-             cin >> t;   // needs extraction operator overloaded for Time
-             if (!cin) {
-                cout << "Invlid time, try agian (HH:MM): ";
+    int getTime() {
+        int mins = -1;
+        if (debug) {
+            Time t(0);
+            cout << "Enter current time: ";
+            do {
                 cin.clear();
-             }
-             else {
-                mins = int(t);
-             }
-             cin.ignore(1000, '\n');
-          } while (mins < 0);
-       }
-       else {
-          time_t t = time(NULL);
-          tm lt = *localtime(&t);
-          mins = lt.tm_hour * 60 + lt.tm_min;
-       }
-       return mins;
+                cin >> t;   // needs extraction operator overloaded for Time
+                if (!cin) {
+                    cout << "Invlid time, try agian (HH:MM): ";
+                    cin.clear();
+                }
+                else {
+                    mins = int(t);
+                }
+                cin.ignore(1000, '\n');
+            } while (mins < 0);
+        }
+        else {
+            time_t t = time(NULL);
+            tm lt = *localtime(&t);
+            mins = lt.tm_hour * 60 + lt.tm_min;
+        }
+        return mins;
     }
-    //int getInt(const char* prompt) {
-    //    if (prompt) cout << prompt << endl;
-
-    //    bool done = false;
-    //    int num;
-    //    while (!done) {
-    //        cin >> num;
-    //        if (cin.fail()) {
-    //            cin.clear();
-    //            cin.ignore(3000, '\n');
-    //            cout << "Bad integer value, try again: ";
-    //        }
-
-    //        else  done = true;
-    //    }
-    //    while (getchar() != '\n')  cout << "Enter only an integer, try again: ";
-
-    //    return num;
-
-    //}
 
 
 
-    //int getInt(const char* prompt) {
-    //    if (prompt) cout << prompt << endl;
-
-    //    bool done = false;
-    //    int num;
-    //    while (!done) {
-    //        cin >> num;
-    //        if (cin.fail()) {
-    //            cin.clear();
-    //            cin.ignore(3000, '\n');
-    //            cout << "Bad integer value, try again: ";
-    //        }
-
-    //        else  done = true;
-    //    }
-    //    while (getchar() != '\n')  cout << "Enter only an integer, try again: ";
-
-    //    return num;
-
-    //}
+    void clear(void) {
+        char junk;
+        do {
+            junk = getchar();
+        } while (junk != '\n');
+    }
 
 
 
 
 
-    //void clear() { while (getchar() != '\n'); }
+    int getInt(const char* prompt)
+    {
+        int value, keeptrying = 1, rc;
+        char after;
+        if (prompt) cout << prompt;
+        do {
+           
+         
+           rc = scanf("%d%c", &value, &after);
+            if (rc == 0) {
+                cout << "Bad integer value, try again: ";
+                clear();
+            }
+            else if (after != '\n') {
+               cout<< "Enter only an integer, try again: ";
+                clear();
+            }
+       
+            else
+                keeptrying = 0;
+        } while (keeptrying == 1);
 
-    //int getInt(const char* prompt) {
-
-    //    if (prompt)  cout << prompt;
-    //    int i = 0; char nl = 0;
-    //    int check = 0;
-    //    do {
-    //        label:check = scanf("%d%c", &i, &nl);
-    //        if (check == 0) {
-    //            cout << "Bad integer value, try again: ";
-    //            clear();
-    //            continue;  //watt to do
-    //            //goto label;
-    //        }
-    //        if (nl != '\n') {
-    //            cout << "Enter only an integer, try again: ";
-    //            clear();
-    //        }
-    //    } while (nl != '\n');
-    //    return i;
-    //}
-
-
-
-
+        return value;
+    }
 
 
 
-    //int getInt(int min, int max, const char* prompt, const char* errorMessage, bool showRangeAtError)    {
-
-    //    int val = getInt();
-    //    if (prompt)  cout << prompt;
-    //    while (val<min || val>max) {
-    //       // if (errorMessage)  cout << errorMessage;
-    //       // if (showRangeAtError == true) cout << "[" << min << "<= value <=" << max << "]";
-    //        printf("(%d<=value<=%d): ", min, max);
-    //        val=getInt();
-
-    //    }
-    //    return val;
-    //}
-
-    //char* getcstr(const char* prompt, std::istream& istr, char delimiter)
-    //{
-    //    return nullptr;
-    //}
+    int getInt(
+        int min,   // minimum acceptable value
+        int max,   // maximum acceptable value
+        const char* prompt,  // User entry prompt
+        const char* errorMessage, // Invalid value error message
+        bool showRangeAtError     // display the range if invalud value entered 
+    )
+    {
 
 
+        int value, keeptrying = 1, rc;
+        char after;
+        if (prompt) cout << prompt;
+        do {
+           
+
+            rc = scanf("%d%c", &value, &after);
+            if (rc == 0) {
+                cout << "Bad integer value, try again: ";
+                clear();
+            }
+            else if (after != '\n') {
+                cout << "Enter only an integer, try again: ";
+                clear();
+            }
+
+
+            else if (value < min || value > max) {
+
+                if (errorMessage)    cout << errorMessage;
+
+                if (showRangeAtError) cout << "[" << min << " <= value <= " << max << "]: ";
+
+            }
 
 
 
 
+            else  keeptrying = 0;
 
+        } while (keeptrying == 1);
+
+        return value;
+    }
+
+
+
+   
 }
     
     
+    //works
     
-    
-    
+//int getInt(
+//    int min,   // minimum acceptable value
+//    int max,   // maximum acceptable value
+//    const char* prompt,  // User entry prompt
+//    const char* errorMessage, // Invalid value error message
+//    bool showRangeAtError     // display the range if invalud value entered 
+//)
+//{
+//
+//
+//    int value, keeptrying = 1, rc;
+//    char after;
+//    if (prompt) cout << prompt;
+//    do {
+//
+//
+//        rc = scanf("%d%c", &value, &after);
+//        if (rc == 0) {
+//            cout << "Bad integer value, try again: ";
+//            clear();
+//        }
+//        else if (after != '\n') {
+//            cout << "Enter only an integer, try again: ";
+//            clear();
+//        }
+//
+//
+//        else if (value < min || value > max) {
+//
+//            if (errorMessage)    cout << errorMessage;
+//
+//            if (showRangeAtError) cout << "[" << min << " <= value <= " << max << "]: ";
+//
+//        }
+//
+//
+//
+//
+//        else  keeptrying = 0;
+//
+//    } while (keeptrying == 1);
+//
+//    return value;
+//}
+
+
     
     
   
