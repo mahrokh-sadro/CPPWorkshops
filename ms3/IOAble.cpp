bbe7d94cf0a440a7dd75460d6d0c11b2a74dc9fe
mahrokh sadro <74023410+mahrokh-sadro@users.noreply.github.com>
2021-03-25 10:39:01 -0400

    Add files via upload

---
+++ ms3/IOAble.cpp
@@ -0,0 +1,31 @@
+/* Citation and Sources...
+Final Project Milestone 2
+Module: IOAble
+Filename: IOAble.h
+Version 1.0
+Author	Mahrokh Sadrolodabaee
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2020/3/23  Preliminary release
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+#include <iostream>
+#include "IOAble.h"
+using namespace std;
+namespace sdds {
+
+	istream& operator>>(istream& is, IOAble& obj)
+	{
+		return obj.read(is);
+	}
+
+	ostream& operator<<(ostream& os, const IOAble& obj)
+	{
+		return obj.write(os);
+	}
+
+}
---
+++ ms3/IOAble.h
@@ -0,0 +1,35 @@
+/* Citation and Sources...
+Final Project Milestone 
+Module: IOAble
+Filename: IOAble.cpp
+Version 1.0
+Author	Mahrokh Sadrolodabaee
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2020/3/25  Preliminary release
+
+-----------------------------------------------------------
+I have done all the coding by myself and only copied the code
+that my professor provided to complete my workshops and assignments.
+-----------------------------------------------------------*/
+
+#ifndef SDDS_IOABLE_H_
+#define SDDS_IOABLE_H_
+
+#include <iostream>
+namespace sdds {
+
+	class IOAble {
+	public:
+		virtual std::ostream& csvWrite(std::ostream& os=std::cout) const = 0;
+		virtual std::istream& csvRead(std::istream& is = std::cin) = 0;
+		virtual std::ostream& write(std::ostream& os = std::cout) const = 0;
+		virtual std::istream& read(std::istream& is = std::cin) = 0;
+		virtual ~IOAble() {};
+
+	};
+	std::istream& operator>>(std::istream& is, IOAble& obj);
+	std::ostream& operator<<(std::ostream& os, const IOAble& obj);
+}
+#endif
