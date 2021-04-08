ff0b83911ac6528bf718881a09872231fff3bd00
mahrokh sadro <74023410+mahrokh-sadro@users.noreply.github.com>
2021-04-08 11:11:25 -0400

    Update TriagePatient.h

--- ms5/TriagePatient.h
+++ ms5/TriagePatient.h
@@ -1,24 +1,4 @@
-//#ifndef SDDS_TRIAGEPATIENT_H
-//#define SDDS_TRIAGEPATIENT_H
-//#include "Patient.h"
-//
-//namespace sdds {
-//    class TriagePatient : public Patient{
-//        private:
-//            char *m_symptoms;
-//        public:
-//            TriagePatient();
-//            ~TriagePatient();
-//            char type() const;
-//            std::istream& read(std::istream& istr);
-//            std::ostream& write(std::ostream& ostr) const;
-//            std::istream& csvRead(std::istream& istr);
-//            std::ostream& csvWrite(std::ostream& ostr) const;
-//    };
-//}
-//#endif // !SDDS_TRIAGEPATIENT_H
-//
-//
+
 /* Citation and Sources...
 Final Project Milestone 4
 Module: TriagePatient
