5735257af8f67c99f4e41b45bb0006ce95ed836a
mahrokh sadro <74023410+mahrokh-sadro@users.noreply.github.com>
2021-04-08 11:09:44 -0400

    Update Patient.h

--- ms5/Patient.h
+++ ms5/Patient.h
@@ -1,36 +1,3 @@
-//#ifndef SDDS_PATIENT_H
-//#define SDDS_PATIENT_H
-//#include "IOAble.h"
-//#include "Ticket.h"
-//#include <iostream>
-//namespace sdds {
-//    class Patient: public IOAble{
-//            char* m_patientName = nullptr;
-//            int m_ohipNum;
-//            Ticket A;
-//            bool m_fileIO;
-//        public:
-//            Patient(int number = 0, bool flag = false);
-//            ~Patient();
-//            Patient(const Patient&) = delete;
-//            void operator=(const Patient&) = delete;
-//            void getSpecs(std::istream&);
-//            virtual char type() const = 0;
-//            bool fileIO() const;
-//            void fileIO(bool);
-//            bool operator ==(char) const;
-//            bool operator ==(const Patient&) const;
-//            void setArrivalTime();
-//            operator Time() const;
-//            int number() const;
-//            std::istream& read(std::istream& istr);
-//            std::ostream& write(std::ostream& ostr) const;
-//            std::istream& csvRead(std::istream& istr);
-//            std::ostream& csvWrite(std::ostream& ostr) const;
-//    };
-//}
-//#endif
-
 
 /* Citation and Sources...
 Final Project Milestone 4
